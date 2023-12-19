0fac2c50409237f5a5b4c4701b022141a17ee9cd
PiraTechnics <devin1younge@gmail.com>
2023-12-18 21:53:53 -0800

    header opacity cleanup

--- app/ui/header-nav.tsx
+++ app/ui/header-nav.tsx
@@ -4,7 +4,7 @@ import { ToolBar } from "./toolbar";
 
 export const HeaderNav = () => {
 	return (
-		<header className="fixed w-full max-w-screen-2xl top-0 z-10 bg-white/40 h-fit shadow-xl text-black">
+		<header className="fixed w-full max-w-screen-2xl top-0 z-10 bg-white/50 h-fit shadow-xl text-black">
 			<nav className="flex px-2 items-center justify-between h-full mx-auto">
 				<CeruleanLogo />
 				<NavLinks />
