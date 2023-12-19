e5b813adf30b77b42e445c33a201408a78e2eaa3
PiraTechnics <devin1younge@gmail.com>
2023-12-18 20:24:26 -0800

    smoother hero animation

--- app/(overview)/page.tsx
+++ app/(overview)/page.tsx
@@ -1,25 +1,60 @@
+"use client";
+
 import Image from "next/image";
 import homeHero from "@/public/matt-hardy-6ArTTluciuA-unsplash.jpg";
 import { ArrowRightCircleIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
+import clsx from "clsx";
 
 export default function Home() {
+	const [loaded, setLoaded] = useState(false);
+
+	const triggerAnimation = () => {
+		setLoaded(true);
+	};
+
 	return (
-		<section className="bg-slate-800 relative text-center text-slate-100">
-			<Image
-				src={homeHero}
-				alt="Lightly rippling ocean waves"
-				className="opacity-70 object-cover"
-			/>
-			<div className="absolute inset-0 top-2/4 overflow-hidden">
-				<div className="text-5xl animate-slideFromLeft">
-					World Class Web Design
+		<>
+			<section className="bg-slate-800 text-center text-slate-100">
+				<div className="relative">
+					<Image
+						onLoad={triggerAnimation}
+						src={homeHero}
+						alt="Lightly rippling ocean waves"
+						className="opacity-70 object-cover"
+					/>
+					<div className="absolute inset-0 top-2/4 overflow-hidden">
+						<div
+							className={clsx(
+								"text-5xl",
+								{ invisible: !loaded },
+								{ "animate-slideFromLeft": loaded }
+							)}
+						>
+							World Class Web Design
+						</div>
+						<div
+							className={clsx(
+								"text-3xl",
+								{ invisible: !loaded },
+								{ "animate-slideFromRight": loaded }
+							)}
+						>
+							Reasonable Prices
+						</div>
+						<button
+							className={clsx(
+								"mt-8 mx-auto flex transform items-center rounded-full bg-gradient-to-r from-cws-primary to-cws-bg-1 px-3 py-2 font-medium text-white transition hover:scale-110 ring-1 ring-cws-bg-1 hover:ring-cws-bg-1/50 focus:outline-none",
+								{ invisible: !loaded },
+								{ "animate-fadeIn": loaded }
+							)}
+						>
+							<span className="px-1">Get Started</span>
+							<ArrowRightCircleIcon className="w-7" />
+						</button>
+					</div>
 				</div>
-				<div className="text-3xl animate-slideFromRight">Reasonable Prices</div>
-				<button className="mt-8 mx-auto flex transform items-center rounded-full bg-gradient-to-r from-cws-primary to-cws-bg-1 px-3 py-2 font-medium text-white transition hover:scale-110 ring-1 ring-cws-bg-1 hover:ring-cws-bg-1/50 focus:outline-none animate-fadeIn">
-					<span className="px-1">Get Started</span>
-					<ArrowRightCircleIcon className="w-7" />
-				</button>
-			</div>
-		</section>
+			</section>
+		</>
 	);
 }
