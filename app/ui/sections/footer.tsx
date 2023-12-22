df0916368e1efcb6438375d40d332b78b9a6cf70
PiraTechnics <devin1younge@gmail.com>
2023-12-21 18:17:33 -0800

    Clean Footer, new icons

--- app/ui/sections/footer.tsx
+++ app/ui/sections/footer.tsx
@@ -1,7 +1,9 @@
-import { MapPinIcon, PhoneIcon, EnvelopeIcon } from "@heroicons/react/24/solid";
-import Facebook from "@/public/facebook.svg";
-import Instagram from "@/public/instagram.svg";
-import LinkedIn from "@/public/linkedin.svg";
+import { PhoneIcon, EnvelopeIcon } from "@heroicons/react/24/solid";
+import Facebook from "@/public/fb-white.svg";
+import Instagram from "@/public/ig-white.svg";
+import LinkedIn from "@/public/li-white.svg";
+import FullLogo from "@/public/cerulean.svg";
+
 import Link from "next/link";
 import Image from "next/image";
 
@@ -19,57 +21,78 @@ export const Footer = () => {
 		{ name: "Facebook", href: "#", icon: Facebook },
 		{ name: "Instagram", href: "#", icon: Instagram },
 	];
+
+	const contact = {
+		Email: "fakename@testmail.net",
+		Phone: "(555) 555-5555",
+	};
+
 	const today = new Date();
 
 	return (
 		<footer className="bg-slate-300/40 py-4 text-white">
-			<div className="flex max-sm:flex-col justify-around max-sm:mx-auto max-sm:items-center max-sm:gap-8">
-				<div id="footer-nav" className="flex flex-col text-lg">
-					{links.map((link) => {
-						return (
-							<Link
-								key={link.name}
-								href={link.href}
-								className="text-lg xl:text-lg py-1 hover:underline "
-							>
-								{link.name}
-							</Link>
-						);
-					})}
+			<div
+				id="footer-content"
+				className="grid lg:grid-cols-4 lg:grid-rows-1 md:grid-cols-2 md:grid-rows-2 grid-rows-4 grid-cols-1 my-2"
+			>
+				<div id="footer-about" className="mb-4 justify-self-center text-center">
+					<div className="mb-4 p-1 mx-auto rounded-full bg-white/60 w-56 md:w-48 lg:w-40">
+						<Image src={FullLogo} alt="Cerulean Web Studio" />
+					</div>
+					<p className="px-4 text-lg sm:px-32 md:px-16 lg:text-sm lg:px-10">
+						We are a full service web agency passionate about delivering an
+						exceptional experience for creatives and small businesses looking
+						for web solutions.
+					</p>
 				</div>
-				<div id="footer-socials" className="flex flex-col justify-center">
-					<span className="text-xl text-center">Find us Online</span>
-					<div className="flex justify-around gap-4">
+				<div id="footer-nav" className="place-self-center">
+					<ul className="list-none text-center">
+						{links.map((link) => {
+							return (
+								<li key={link.name}>
+									<Link
+										href={link.href}
+										className="text-xl lg:text-lg py-1 hover:underline "
+									>
+										{link.name}
+									</Link>
+								</li>
+							);
+						})}
+					</ul>
+				</div>
+				<div id="footer-location" className="justify-self-center text-center">
+					<div className="text-2xl my-4">Location</div>
+					<div>Los Angeles</div>
+					<p>420 Nowhere Street, Suite 690</p>
+					<p>Sherman Oaks, CA 91411</p>
+				</div>
+				<div id="footer-socials" className="justify-self-center text-center">
+					<div className="text-2xl my-4">Contact</div>
+					<div className="flex gap-2 my-4 justify-center">
 						{socials.map((link) => {
 							return (
 								<Link
 									key={link.name}
 									href={link.href}
-									className="text-lg xl:text-lg py-1 hover:underline "
+									className="p-1.5 rounded-full bg-slate-700"
 								>
-									<Image src={link.icon} alt={link.name} width={32} />
+									<Image src={link.icon} alt={link.name} />
 								</Link>
 							);
 						})}
 					</div>
-				</div>
-				<div id="footer-contact" className="flex flex-col gap-4 py-4">
-					<div className="flex gap-2">
-						<MapPinIcon width={30} />
-						<p>
-							2222 Someplace Blvd #42 <br /> Nowheresville, CA
-						</p>
+					<div className="mb-2">
+						<PhoneIcon width={24} className="inline me-2" />
+						{contact.Phone}
 					</div>
-					<div className="flex gap-2">
-						<PhoneIcon width={28} />
-						<p>(555) 555-5555</p>
-					</div>
-					<div className="flex gap-2">
-						<EnvelopeIcon width={28} />
-						<p>fakename@testmail.net</p>
+					<div>
+						<EnvelopeIcon width={24} className="inline me-2" />
+						{contact.Email}
 					</div>
 				</div>
 			</div>
+			<hr className="mx-8" />
 			<div id="footer-copyright" className="text-sm text-center mt-4">
 				&copy; {today.getFullYear()} - Cerululean Web Studio
 			</div>
